dfb3ab5c543b5e8ab4c6a0c92ff82d8dadd2ca43
Evsyukov Denis <denis@evsyukov.org>
2025-01-08 21:13:23 +0300

    wip

--- go.mod
+++ go.mod
@@ -6,10 +6,11 @@ require (
 	github.com/carlmjohnson/requests v0.24.3
 	github.com/cenkalti/backoff/v5 v5.0.0
 	github.com/tidwall/gjson v1.18.0
+	github.com/tidwall/sjson v1.2.5
 )
 
 require (
 	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
 	golang.org/x/net v0.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log/slog"
 	"os"
 	"strconv"
 	"time"
@@ -10,28 +10,52 @@ import (
 	"github.com/carlmjohnson/requests"
 	"github.com/cenkalti/backoff/v5"
 	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
 )
 
+var logger *slog.Logger
+
+func init() {
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+}
+
 func main() {
-	consumer_key := os.Getenv("POCKET_CONSUMER_KEY")
-	if consumer_key == "" {
-		fmt.Println("POCKET_CONSUMER_KEY is not set")
+	pocketConsumerKey := os.Getenv("POCKET_CONSUMER_KEY")
+	if pocketConsumerKey == "" {
+		logger.Error("POCKET_CONSUMER_KEY is not set")
 		os.Exit(1)
 	}
-	access_token := os.Getenv("POCKET_ACCESS_TOKEN")
-	if access_token == "" {
-		fmt.Println("POCKET_ACCESS_TOKEN is not set")
+	pocketAccessToken := os.Getenv("POCKET_ACCESS_TOKEN")
+	if pocketAccessToken == "" {
+		logger.Error("POCKET_ACCESS_TOKEN is not set")
+		os.Exit(1)
+	}
+	linkdingAccessToken := os.Getenv("LINKDING_ACCESS_TOKEN")
+	if linkdingAccessToken == "" {
+		logger.Error("LINKDING_ACCESS_TOKEN is not set")
 		os.Exit(1)
 	}
 
+	if err := process(
+		pocketConsumerKey,
+		pocketAccessToken,
+		linkdingAccessToken,
+	); err != nil {
+		logger.Error("Error", "error", err)
+		os.Exit(1)
+	}
+
+}
+
+func process(pocketConsumerKey, pocketAccessToken, linkdingAccessToken string) error {
 	since := time.Now().Add(30 * time.Minute).Unix()
 
 	operation := func() (string, error) {
 		var dat string
 		err := requests.
 			URL("https://getpocket.com/v3/get").
-			Param("consumer_key", consumer_key).
-			Param("access_token", access_token).
+			Param("consumer_key", pocketConsumerKey).
+			Param("access_token", pocketAccessToken).
 			Param("since", strconv.FormatInt(since, 10)).
 			ToString(&dat).
 			Fetch(context.Background())
@@ -44,18 +68,44 @@ func main() {
 
 	result, err := backoff.Retry(context.TODO(), operation, backoff.WithBackOff(backoff.NewExponentialBackOff()))
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		logger.Error("Error", "error", err)
+		return err
 	}
 
-	fmt.Println(result)
+	if gjson.Get(result, "status").Int() == 2 {
+		logger.Info("No new items")
+		return nil
+	}
+
 	gjson.Get(result, "list").ForEach(func(key, value gjson.Result) bool {
 		u := value.Get("resolved_url")
 		if u.Exists() {
-			fmt.Println(u.String())
+			value, _ := sjson.Set("", "url", u.String())
+			operation := func() (string, error) {
+				err := requests.
+					URL("https://links.evsyukov.org/api/bookmarks/").
+					BodyBytes([]byte(value)).
+					Header("auth", "Authorization "+linkdingAccessToken).
+					ContentType("application/json").
+					Fetch(context.Background())
+				if err != nil {
+					return "", backoff.RetryAfter(1)
+				}
+
+				return "", nil
+			}
+
+			_, err := backoff.Retry(context.TODO(), operation, backoff.WithBackOff(backoff.NewExponentialBackOff()))
+			if err != nil {
+				logger.Error("Error", "error", err)
+				return false
+			}
 		}
+
+		logger.Info("Added", "url", u.String())
 
 		return true
 	})
 
+	return nil
 }
